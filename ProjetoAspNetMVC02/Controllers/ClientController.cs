2557b40e31cfb7ac9e9ddef2a81f32f01c1a05b3
joaogqueiroz <joaogilberto350@hotmail.com>
2021-08-08 21:36:28 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoAspNetMVC02/Controllers/ClientController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoAspNetMVC02.Entities;
+using ProjetoAspNetMVC02.Interfaces;
+using ProjetoAspNetMVC02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoAspNetMVC02.Controllers
+{
+    public class ClientController : Controller
+    {
+        private IClientRepository _clientRepository;
+        public ClientController(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(ClientRegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var client = new Client();
+
+                    client.Name = model.Name;
+                    client.Email = model.Email;
+                    _clientRepository.Insert(client);
+
+                    TempData["Messege"] = $"Client {client.Name}, was registred successfully.";
+                }
+                catch (Exception e)
+                {
+
+                    TempData["Messege"] = e.Message;
+                }
+                
+            }
+            return View();
+
+        }
+
+        public IActionResult Consult()
+        {
+            try
+            {
+                TempData["Consult"] = _clientRepository.Consult();
+            }
+            catch (Exception e)
+            {
+
+                TempData["Messege"] = e.Message;
+            }
+            return View();
+        }
+    }
+}
---
+++ ProjetoAspNetMVC02/Repositories/ClientRepository.cs
@@ -0,0 +1,76 @@
+﻿using Dapper;
+using ProjetoAspNetMVC02.Entities;
+using ProjetoAspNetMVC02.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoAspNetMVC02.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private string _connectionString;
+
+        public ClientRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void Alter(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Client> Consult()
+        {
+            var query = @"
+                        SELECT * FROM CLIENT
+                        ORDER BY NAME
+                        ";
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.Query<Client>(query).ToList();
+            }
+        }
+
+        public void Delete(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Client GetByID(Guid clientId)
+        {
+            var query = @"
+                        SELECT * FROM CLIENT
+                        WHERE
+                        CLIENTID = @clientId
+                        ";
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.Query(query, clientId).FirstOrDefault();
+            }
+        }
+
+        public void Insert(Client client)
+        {
+            var query = @"
+                        INSERT INTO CLIENT(
+                            CLIENTID,
+                            NAME,
+                            EMAIL,
+                            REGISTRATIONDATA)
+                        VALUES(
+                            NEWID(),
+                            @Name,
+                            @Email,
+                            GETDATE())
+                        ";
+            using(var connection = new SqlConnection(_connectionString))
+            {
+                connection.Execute(query, client);
+            }
+        }
+    }
+}
